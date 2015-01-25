b030a3c418270d982828e1deb8e87995a1ffc4c6
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2015-01-25 20:28:25 +0400

    Commit addon files

---
+++ ui.py
@@ -0,0 +1,68 @@
+# ##### BEGIN MIT LICENSE BLOCK #####
+#
+# Copyright (c) 2012 Mikhail Rachinskiy
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# ##### END MIT LICENSE BLOCK #####
+
+import bpy
+from bpy.types import (Panel, Menu)
+
+
+class BooltronPanel(Panel):
+	
+	bl_label = "Booltron"
+	bl_idname = "OBJECT_PT_BOOLTRON"
+	bl_space_type = 'VIEW_3D'
+	bl_region_type = 'TOOLS'
+	bl_category = "Booltron"
+
+	@classmethod
+	def poll(cls, context):
+		obj = context.active_object
+		return (obj and context.mode == 'OBJECT')
+
+	def draw(self, context):
+		layout = self.layout
+
+		col = layout.column(align=True)
+		col.operator("booltron.union")
+		col.operator("booltron.difference")
+		col.operator("booltron.intersect")
+		
+		col.separator()
+		col.operator("booltron.separate")
+
+
+class BooltronMenu(Menu):
+
+	bl_label = "Booltron"
+	bl_idname = "OBJECT_PT_BOOLTRON_MENU"
+
+	def draw(self, context):
+		layout = self.layout
+		layout.operator_context = 'INVOKE_REGION_WIN'
+
+		layout.operator("booltron.union")
+		layout.operator("booltron.difference")
+		layout.operator("booltron.intersect")
+
+		layout.separator()
+		layout.operator("booltron.separate")
