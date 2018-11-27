9748e0b878c98291e52c62ebab1790cec0e61986
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2018-11-27 18:46:15 +0400

    Fix (c) description

--- translations.py
+++ translations.py
@@ -1,6 +1,6 @@
 # ##### BEGIN GPL LICENSE BLOCK #####
 #
-#  JewelCraft jewelry design toolkit for Blender.
+#  Booltron super add-on for super fast booleans.
 #  Copyright (C) 2014-2018  Mikhail Rachinskiy
 #
 #  This program is free software: you can redistribute it and/or modify
