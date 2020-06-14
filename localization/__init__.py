66d03ea79e4a23229d5d4da29c7805a055febc19
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2020-06-14 20:58:18 +0400

    Refactor localization
    * Store dictionaries in json files instead of py modules.
    * Modular system: remove or add new dictionaries wihtout chaning the code.

---
+++ localization/__init__.py
@@ -0,0 +1,42 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  Booltron super add-on for super fast booleans.
+#  Copyright (C) 2014-2020  Mikhail Rachinskiy
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+
+def _get_dicts():
+    import os
+    import json
+
+    for entry in os.scandir(os.path.dirname(__file__)):
+        if entry.is_file() and entry.name.endswith(".json"):
+            with open(entry, "r", encoding="utf-8") as file:
+                yield json.load(file)
+
+
+def _convert(dictionary):
+    d = {}
+
+    for ctxt, msgs in dictionary.items():
+        for msg_key, msg_translation in msgs.items():
+            d[(ctxt, msg_key)] = msg_translation
+
+    return d
+
+
+DICTIONARY = {k: _convert(v) for k, v in _get_dicts()}
