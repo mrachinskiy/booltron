ac2f0875cb851732194103334fa09c793d2c0bfd
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2020-07-04 22:05:47 +0400

    Relocate preferences UI to ui module

--- ui.py
+++ ui.py
@@ -118,3 +118,60 @@ class VIEW3D_PT_booltron_nondestructive(Setup, Panel):
         col.operator("object.booltron_nondestructive_intersect", icon_value=self.icon_get("NONDESTR_INTERSECT"))
 
         layout.operator("object.booltron_nondestructive_remove", icon_value=self.icon_get("NONDESTR_REMOVE"))
+
+
+# Preferences
+# ---------------------------
+
+
+def prefs_ui(self, context):
+    props_wm = context.window_manager.booltron
+    active_tab = props_wm.prefs_active_tab
+
+    layout = self.layout
+    layout.use_property_split = True
+    layout.use_property_decorate = False
+
+    split = layout.split(factor=0.25)
+    col = split.column()
+    col.use_property_split = False
+    col.scale_y = 1.3
+    col.prop(props_wm, "prefs_active_tab", expand=True)
+
+    box = split.box()
+
+    if active_tab == "DESTRUCTIVE":
+        col = box.column()
+        col.prop(self, "destr_double_threshold")
+
+        row = col.row(heading="Correct Position")
+        row.prop(self, "destr_use_pos_offset", text="")
+        sub = row.row()
+        sub.active = self.destr_use_pos_offset
+        sub.prop(self, "destr_pos_offset", text="")
+
+        col.prop(self, "merge_distance")
+        col.prop(self, "cleanup")
+        col.prop(self, "triangulate")
+
+    elif active_tab == "NONDESTRUCTIVE":
+        col = box.column()
+        col.prop(self, "nondestr_double_threshold")
+
+        row = col.row(heading="Correct Position")
+        row.prop(self, "nondestr_use_pos_offset", text="")
+        sub = row.row()
+        sub.active = self.nondestr_use_pos_offset
+        sub.prop(self, "nondestr_pos_offset", text="")
+
+        box.label(text="Viewport Display")
+        col = box.column()
+        col.prop(self, "display_secondary")
+        col.prop(self, "display_combined")
+
+    elif active_tab == "UI":
+        col = box.column()
+        col.prop(self, "theme_icon")
+
+    elif active_tab == "UPDATES":
+        mod_update.prefs_ui(self, box)
